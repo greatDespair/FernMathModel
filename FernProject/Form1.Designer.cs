57ddf1a4f09808a2948e98594ab600adcfabda7f
Sergey <akmashev2002@gmail.com>
2022-09-19 20:00:51 +0300

    first

---
+++ FernProject/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace FernProject
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ModelView = new System.Windows.Forms.PictureBox();
+            this.CheckFern = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ModelView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ModelView
+            // 
+            this.ModelView.Location = new System.Drawing.Point(391, 12);
+            this.ModelView.Name = "ModelView";
+            this.ModelView.Size = new System.Drawing.Size(554, 493);
+            this.ModelView.TabIndex = 0;
+            this.ModelView.TabStop = false;
+            // 
+            // CheckFern
+            // 
+            this.CheckFern.Location = new System.Drawing.Point(52, 42);
+            this.CheckFern.Name = "CheckFern";
+            this.CheckFern.Size = new System.Drawing.Size(264, 81);
+            this.CheckFern.TabIndex = 1;
+            this.CheckFern.Text = "button1";
+            this.CheckFern.UseVisualStyleBackColor = true;
+            this.CheckFern.Click += new System.EventHandler(this.CheckFern_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(957, 685);
+            this.Controls.Add(this.CheckFern);
+            this.Controls.Add(this.ModelView);
+            this.Name = "MainForm";
+            this.Text = "FernProject v0.1";
+            ((System.ComponentModel.ISupportInitialize)(this.ModelView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ModelView;
+        private System.Windows.Forms.Button CheckFern;
+    }
+}
+
---
+++ FernProject/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FernProject
+{
+    public partial class MainForm : Form
+    {
+        
+        public MainForm()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private Graphics g;
+        private Pen p;
+
+        private void FirstLine(int x, int y, double a, double b)
+        {
+            g.DrawLine(p, x, y, (int)Math.Round(x + a * Math.Cos(b)), (int)Math.Round(y - a * Math.Sin(b)));
+        }
+
+        private void Draw(int x, int y, double a, double b)
+        {
+            if (a > 1)
+            {
+                FirstLine(x, y, a, b);
+                x = (int)Math.Round(x + a * Math.Cos(b));
+                y = (int)Math.Round(y - a * Math.Sin(b));
+                Draw(x, y, a * 0.5, b - 10 * Math.PI / 30);
+                Draw(x, y, a * 0.5, b + 10 * Math.PI / 30);
+                Draw(x, y, a * 0.7, b + Math.PI / 30);
+            }
+        }
+
+        private void CheckFern_Click(object sender, EventArgs e)
+        {
+            g = ModelView.CreateGraphics();
+            p = new Pen(Color.Black);
+            g.FillRectangle(Brushes.White, 0, 0, ModelView.Width, ModelView.Height);
+            Draw(100, 400, 500, Math.PI /4);
+        }
+    }
+}
