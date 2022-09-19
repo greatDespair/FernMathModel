248a87f47cb1c146e8702d69ab20146d9f7d4632
Despair <akmashev2002@gmail.com>
2022-09-19 21:43:52 +0300

    Тестовые отрисовки

--- FernProject/Form1.cs
+++ FernProject/Form1.cs
@@ -23,30 +23,30 @@ namespace FernProject
         private Graphics g;
         private Pen p;
 
-        private void FirstLine(int x, int y, double a, double b)
+        private void DrawLine(int x, int y, double a, double b)
         {
-            g.DrawLine(p, x, y, (int)Math.Round(x + a * Math.Cos(b)), (int)Math.Round(y - a * Math.Sin(b)));
+            g.DrawLine(p, x, y, (int)Math.Round(x + 0.2*a * Math.Cos(b)), (int)Math.Round(y - 0.2*a * Math.Sin(b)));
         }
 
         private void Draw(int x, int y, double a, double b)
         {
-            if (a > 1)
+            if (a > 5)
             {
-                FirstLine(x, y, a, b);
-                x = (int)Math.Round(x + a * Math.Cos(b));
-                y = (int)Math.Round(y - a * Math.Sin(b));
-                Draw(x, y, a * 0.5, b - 10 * Math.PI / 30);
-                Draw(x, y, a * 0.5, b + 10 * Math.PI / 30);
-                Draw(x, y, a * 0.7, b + Math.PI / 30);
+                DrawLine(x, y, a, b);
+                x = (int)Math.Round(x + 0.2 * a * Math.Cos(b));
+                y = (int)Math.Round(y - 0.2 * a * Math.Sin(b));
+                Draw(x, y, a * 0.3, b - 10 * Math.PI / 30);
+                Draw(x, y, a * 0.3, b + 10 * Math.PI / 30);
+                Draw(x, y, a * 0.9, b + Math.PI / 30);
             }
         }
 
         private void CheckFern_Click(object sender, EventArgs e)
         {
             g = ModelView.CreateGraphics();
-            p = new Pen(Color.Black);
+            p = new Pen(Color.Green);
             g.FillRectangle(Brushes.White, 0, 0, ModelView.Width, ModelView.Height);
-            Draw(100, 400, 500, Math.PI /4);
+            Draw(0, 500, 400, Math.PI /4);
         }
     }
 }
